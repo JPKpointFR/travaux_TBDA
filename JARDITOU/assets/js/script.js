3db0da4ce6812d0cba65bd72077b08bb0059d571
Adama <adam.cis345@gmail.com>
2023-02-16 16:29:18 +0100

    création script alert()

---
+++ JARDITOU/assets/js/script.js
@@ -0,0 +1,28 @@
+/* 
+Créer un script qui demande successivement à l'utilisateur son nom puis son prénom. La page affichera les informations sur l'utilisateur via une "alert()".
+*/
+
+const nom = window.prompt("Saisissez votre nom");
+const prenom = window.prompt("Saisissez votre prénom");
+console.log(nom);
+console.log(prenom);
+
+if (nom && prenom) {
+  window.alert("Bonjour " + nom + " " + prenom);
+}
+
+/*
+Ecrire un programme qui calcule le produit de deux nombres entrés par l'utilisateur et affiche le résultat.
+*/
+
+
+
+
+
+
+/*
+Ecrire un programme qui convertit des degrés Celsius en degrés Fahrenheit en utilisant la formule suivante : F = (C*9/5) + 32. Le programme demandera la température en degrés Celsius à l'utilisateur et affichera la température convertie en degrés Fahrenheit.
+*/
+
+
+
